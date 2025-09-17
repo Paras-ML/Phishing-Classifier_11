42306b20839bc6a3300827cbfadcb76029834907
Deepika Sharma <sharmaparas23@gmail.com>
2025-09-17 09:57:13 +0530

    vizual code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Phishing-Classifier_11
 This is my machine learning project 1
+
+
+'''pip install -e .''' for run setup.py
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="ParasSharma",
+    author_email="sharmaparas23@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
